ac78c88b535bf79bc9ba511d6211c12c7fddb7ba
janejiunkim <janejiunkim@gmail.com>
2021-09-03 15:12:21 -0700

    Create env variable and add session and flash middleware

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
-'use strict';
-const bcrypt = require('bcrypt');
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const bcrypt = require("bcrypt");
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -14,56 +12,58 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  
-  User.init({
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        len: {
-          args: [1, 99],
-          msg: 'Name must be between 1 and 99 characters'
-        }
-      }
-    }, 
-    email: {
-      type: DataTypes.STRING,
-      calidate: {
-        isEmail: {
-          msg: 'Invalid email'
-        }
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        len: {
-          args: [8,99],
-          msg: 'Password must be between 8 and 99 characters'
-        }
-      }
   }
-}, {
-    sequelize,
-    modelName: 'User',
-  });
-  User.addHook('beforeCreate', (pendingUser) => {
+
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 99],
+            msg: "Name must be between 1 and 99 characters",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        calidate: {
+          isEmail: {
+            msg: "Invalid email",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [8, 99],
+            msg: "Password must be between 8 and 99 characters",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  User.addHook("beforeCreate", (pendingUser) => {
     let hash = bcrypt.hashSync(pendingUser.password, 12);
     pendingUser.password = hash;
   });
-   // Check the password on Sign-In and compare it to the hashed password in the DB
-User.prototype.validPassword = function(typedPassword) {
-  let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+  // Check the password on Sign-In and compare it to the hashed password in the DB
+  User.prototype.validPassword = function (typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
 
-  return isCorrectPassword;
-}
-// return an object from the database of the user without the encrypted password
-User.prototype.toJSON = function() {
-  let userData = this.get(); 
-  delete userData.password; // it doesn't delete password from database, only removes it. 
-  
-  return userData;
-}
+    return isCorrectPassword;
+  };
+  // return an object from the database of the user without the encrypted password
+  User.prototype.toJSON = function () {
+    let userData = this.get();
+    delete userData.password; // it doesn't delete password from database, only removes it.
+
+    return userData;
+  };
   return User;
 };
-
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const flash = require('connect-flash');
 
+const session = require('express-session');
 const SECRET_SESSION = process.env.SECRET_SESSION;
+console.log(SECRET_SESSION);
+
+console.log(process.env);
+
 
 app.set('view engine', 'ejs');
 
@@ -11,6 +17,10 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+app.get('/', (req,res) => {
+  res.render('index');
+})
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
